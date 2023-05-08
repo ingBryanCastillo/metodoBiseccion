b4f1c86c4cb2d1f7542441c2e7d691d13ad50a78
trstr <trstr@DESKTOP-395GO8Q>
2023-05-08 01:40:05 -0600

    Todo el metodo terminado

--- src/Metodo.java
+++ src/Metodo.java
@@ -1,12 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Alex
- */
 public class Metodo {
     private Funcion fun;
     double x;
